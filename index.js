c5cac39fe2bfd5884a9a5d597176c4dc63cd974e
Ali <alikamali0328@gmail.com>
2024-10-13 15:51:36 +0330

    Step 5: Fetching product information from API and showing it on the site

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
 import { getCookie } from "./utils/cookie.js";
+import { getData } from "./utils/httpReq.js";
+import { shortenText } from "./utils/strinFunc.js";
 
 const loginButton = document.getElementById("login");
 const dashboardButton = document.getElementById("dashboard");
+const mainContent = document.getElementById("products");
 
-const init = () => {
-  const cookie = getCookie();
+const showProducts = (products) => {
+  mainContent.innerHTML = "";
   
+  console.log(products);
+  products.forEach(product => {
+    const jsx = `
+      <div>
+        <img alt=${product.title} src=${product.image}/>
+        <h4>${shortenText(product.title)}</h4>
+        <div id="price">
+          <p>$ ${product.price}</p>
+          <button>
+            Buy
+            <i class="fa-solid fa-cart-shopping"></i>
+          </button>
+        </div>
+        <div id="rate">
+          <i class="fa-solid fa-star"></i>
+          <span>${product.rating.rate}</span>
+        </div>
+        <div id="count">
+          <i class="fa-solid fa-user"></i>
+          <span>${product.rating.count}</span>
+        </div>
+      </div>
+    `
+
+    mainContent.innerHTML += jsx
+  });
+};
+
+const init = async () => {
+  const cookie = getCookie();
+
   if (cookie) {
-    loginButton.style.display ="none"
+    loginButton.style.display = "none";
   } else {
-    dashboardButton.style.display = "none"
+    dashboardButton.style.display = "none";
   }
+
+  const allProducts = await getData("products");
+  showProducts(allProducts);
 };
 
 document.addEventListener("DOMContentLoaded", init);
